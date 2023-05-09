1577675c6d487988f5b0960b235e1363fb088140
bekirdogru00 <115629168+bekirdogru00@users.noreply.github.com>
2023-05-09 12:17:13 +0300

    Musteri Bilgileri sqli ve okuma komutları yapıldı

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,47 @@
+using NPOI.SS.Formula.Functions;
+using System.Data.SqlClient;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private string button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection baglanti=null;
+            try
+            {
+                baglanti = new SqlConnection(Data Source = DELL\SQLEXPRESS; Initial Catalog = DbSporsalonu; Integrated Security = True);
+                baglanti.Open();
+
+                SqlCommand sqlKomut = new SqlCommand("SELECT MusteiID,MusteriAdi,TelNo,KanGrubu,Cinsiyet FROM MusteriBilgi",baglanti);
+                SqlDataReader sqlDataReader = sqlKomut.ExecuteReader();
+                while (sqlDataReader.Read())
+                {
+                    string MusteriID = sqlDataReader[0].ToString();
+                    string MusteriAdi = sqlDataReader[1].ToString();
+                    string TelNo = sqlDataReader[2].ToString();
+                    string KanGrubu = sqlDataReader[3].ToString();
+                    string Cinsiyet = sqlDataReader[4].ToString();
+                    richTextBox1.Text = richTextBox1.Text + MusteriID + " " + MusteriAdi + "tel no : " + TelNo + "kangrubu:" + KanGrubu + "cinsiyet:" + Cinsiyet + "\n";
+
+                }
+                catch (Exception ex)
+               
+
+            {
+                MessageBox.Show("hata oluþtu" + ex.ToString());
+            }
+            finally
+
+            { if (baglanti != null)
+                baglanti.Close();}
+
+
+            }
+        }
+    }
