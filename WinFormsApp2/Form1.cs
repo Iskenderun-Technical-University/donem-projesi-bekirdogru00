89b385491e86f6eb25417597ab580c084edcb0a3
bekirdogru00 <115629168+bekirdogru00@users.noreply.github.com>
2023-05-23 23:13:55 +0300

    ---Form designer hatası

--- WinFormsApp2/Form1.cs
+++ WinFormsApp2/Form1.cs
@@ -1,6 +1,22 @@
+using System.Windows.Forms;
+
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
 
 using System.Data.SqlClient;
-using System.Windows.Forms;
 
 namespace WinFormsApp1
 {
