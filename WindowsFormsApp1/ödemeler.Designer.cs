1b571ab1167bebd9492247b06a7486db2c26c669
bekirdogru00 <115629168+bekirdogru00@users.noreply.github.com>
2023-06-05 00:27:10 +0300

    KODLAMA TAM ANLAMI İLE BİTTİ UFAK TASARIM SONRASI TAMAMİYLE BİTECEK

--- WindowsFormsApp1/ödemeler.Designer.cs
+++ WindowsFormsApp1/ödemeler.Designer.cs
@@ -30,10 +30,9 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ödemeler));
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.ucrettb = new System.Windows.Forms.TextBox();
+            this.Periyot = new System.Windows.Forms.DateTimePicker();
             this.label2 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
@@ -42,6 +41,7 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.adsoyadcb = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -58,13 +58,6 @@
             this.label1.TabIndex = 22;
             this.label1.Text = "AD SOYAD";
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(205, 143);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(121, 20);
-            this.textBox1.TabIndex = 21;
-            // 
             // label6
             // 
             this.label6.AutoSize = true;
@@ -75,20 +68,20 @@
             this.label6.TabIndex = 27;
             this.label6.Text = "AYLIK ÜCRET";
             // 
-            // textBox6
+            // ucrettb
             // 
-            this.textBox6.Location = new System.Drawing.Point(205, 189);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(121, 20);
-            this.textBox6.TabIndex = 26;
+            this.ucrettb.Location = new System.Drawing.Point(205, 189);
+            this.ucrettb.Name = "ucrettb";
+            this.ucrettb.Size = new System.Drawing.Size(121, 20);
+            this.ucrettb.TabIndex = 26;
             // 
-            // dateTimePicker1
+            // Periyot
             // 
-            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker1.Location = new System.Drawing.Point(177, 93);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(149, 20);
-            this.dateTimePicker1.TabIndex = 28;
+            this.Periyot.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.Periyot.Location = new System.Drawing.Point(177, 82);
+            this.Periyot.Name = "Periyot";
+            this.Periyot.Size = new System.Drawing.Size(149, 20);
+            this.Periyot.TabIndex = 28;
             // 
             // label2
             // 
@@ -110,6 +103,7 @@
             this.button3.TabIndex = 33;
             this.button3.Text = "ÖDEME";
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
@@ -126,9 +120,9 @@
             // 
             this.button1.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button1.Location = new System.Drawing.Point(129, 328);
+            this.button1.Location = new System.Drawing.Point(139, 308);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(82, 48);
+            this.button1.Size = new System.Drawing.Size(93, 31);
             this.button1.TabIndex = 34;
             this.button1.Text = "GERİ";
             this.button1.UseVisualStyleBackColor = false;
@@ -146,9 +140,9 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(86, 328);
+            this.pictureBox2.Location = new System.Drawing.Point(99, 308);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(47, 48);
+            this.pictureBox2.Size = new System.Drawing.Size(47, 31);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 36;
             this.pictureBox2.TabStop = false;
@@ -173,12 +167,21 @@
             this.pictureBox3.TabIndex = 38;
             this.pictureBox3.TabStop = false;
             // 
+            // adsoyadcb
+            // 
+            this.adsoyadcb.FormattingEnabled = true;
+            this.adsoyadcb.Location = new System.Drawing.Point(205, 129);
+            this.adsoyadcb.Name = "adsoyadcb";
+            this.adsoyadcb.Size = new System.Drawing.Size(121, 21);
+            this.adsoyadcb.TabIndex = 39;
+            // 
             // ödemeler
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(810, 450);
+            this.Controls.Add(this.adsoyadcb);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.pictureBox2);
@@ -187,13 +190,13 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.Periyot);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.ucrettb);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
             this.Name = "ödemeler";
             this.Text = "ödemeler";
+            this.Load += new System.EventHandler(this.ödemeler_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -206,10 +209,9 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox ucrettb;
+        private System.Windows.Forms.DateTimePicker Periyot;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
@@ -218,5 +220,6 @@
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.ComboBox adsoyadcb;
     }
 }
--- WindowsFormsApp1/ödemeler.cs
+++ WindowsFormsApp1/ödemeler.cs
@@ -7,14 +7,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Diagnostics.Eventing.Reader;
+
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
     public partial class ödemeler : Form
     {
+        SqlConnection bagla = new SqlConnection("Data Source=DELL\\SQLEXPRESS;Initial Catalog=kayitekle;Integrated Security=True");
+
         public ödemeler()
         {
             InitializeComponent();
+        }
+
+        private void FillName()
+        {
+            bagla.Open();
+            SqlCommand komut = new SqlCommand("SELECT adsoyad FROM kayitekle", bagla);
+            SqlDataReader rdr = komut.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Columns.Add("adsoyad", typeof(string));
+            dt.Load(rdr);
+            adsoyadcb.ValueMember = "adsoyad";
+            adsoyadcb.DataSource = dt;
+            bagla.Close();
+        }
+
+        private void uyeler()
+        {
+            bagla.Open();
+            string query = "SELECT * FROM OdemeTbl";
+            SqlDataAdapter sda = new SqlDataAdapter(query, bagla);
+            SqlCommandBuilder builder = new SqlCommandBuilder(sda);
+            var ds = new DataSet();
+            sda.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+            bagla.Close();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -22,6 +57,41 @@ namespace WindowsFormsApp1
             this.Hide();
             anasayfa an1 = new anasayfa();
             an1.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (adsoyadcb.Text == "" || ucrettb.Text == "")
+            {
+                MessageBox.Show("Eksik bilgi");
+            }
+            else
+            {
+                string odemeperiyot = Periyot.Value.Month.ToString() + Periyot.Value.Year.ToString();
+                bagla.Open();
+                SqlDataAdapter sda = new SqlDataAdapter("SELECT count(*) FROM OdemeTbl WHERE Ouye = '" + adsoyadcb.SelectedValue.ToString() + "' AND Oay = '" + odemeperiyot + "'", bagla);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                if (dt.Rows[0][0].ToString() == "1")
+                {
+                    MessageBox.Show("Ödeme Zaten Yapıldı");
+                }
+                else
+                {
+                    string query = "INSERT INTO OdemeTbl VALUES ('" + odemeperiyot + "', '" + adsoyadcb.SelectedValue.ToString() + "', " + ucrettb.Text + ")";
+                    SqlCommand komut = new SqlCommand(query, bagla);
+                    komut.ExecuteNonQuery();
+                    MessageBox.Show("Başarıyla Ödendi. Bizi tercih ettiğiniz için teşekkürler");
+                }
+                bagla.Close();
+                uyeler();
+            }
+        }
+
+        private void ödemeler_Load(object sender, EventArgs e)
+        {
+            FillName();
+            uyeler();
         }
     }
 }
