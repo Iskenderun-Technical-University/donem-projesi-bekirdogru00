a2a5758ba2162373a68ed662fb437da3e9836ff5
bekirdogru00 <115629168+bekirdogru00@users.noreply.github.com>
2023-06-04 16:35:24 +0300

    tasarımda değişiklikler ve silme işlemi kodları**

--- WindowsFormsApp1/guncellemesilme.Designer.cs
+++ WindowsFormsApp1/guncellemesilme.Designer.cs
@@ -42,31 +42,31 @@
             this.yaştb = new System.Windows.Forms.TextBox();
             this.telnotb = new System.Windows.Forms.TextBox();
             this.adsoyadtb = new System.Windows.Forms.TextBox();
+            this.kayitekleDataSet = new WindowsFormsApp1.kayitekleDataSet();
             this.kangrubucb = new System.Windows.Forms.ComboBox();
             this.button3 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.label7 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.kayitekleDataSet = new WindowsFormsApp1.kayitekleDataSet();
             this.kayitekleDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.button2 = new System.Windows.Forms.Button();
+            this.kayitekleDataSetBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.kayitekleDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.kayitekleDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kayitekleDataSetBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button1.Location = new System.Drawing.Point(82, 359);
+            this.button1.Location = new System.Drawing.Point(397, 70);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(118, 41);
             this.button1.TabIndex = 27;
             this.button1.Text = "GÜNCELLE";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // cinsiyetcb
             // 
@@ -74,7 +74,7 @@
             this.cinsiyetcb.Items.AddRange(new object[] {
             "BAY",
             "BAYAN"});
-            this.cinsiyetcb.Location = new System.Drawing.Point(174, 251);
+            this.cinsiyetcb.Location = new System.Drawing.Point(174, 190);
             this.cinsiyetcb.Name = "cinsiyetcb";
             this.cinsiyetcb.Size = new System.Drawing.Size(121, 21);
             this.cinsiyetcb.TabIndex = 26;
@@ -83,7 +83,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Himalaya", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(13, 303);
+            this.label6.Location = new System.Drawing.Point(13, 219);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(91, 16);
             this.label6.TabIndex = 25;
@@ -93,7 +93,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Himalaya", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(13, 254);
+            this.label5.Location = new System.Drawing.Point(13, 193);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(63, 16);
             this.label5.TabIndex = 24;
@@ -103,7 +103,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Himalaya", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(13, 212);
+            this.label4.Location = new System.Drawing.Point(13, 168);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(81, 16);
             this.label4.TabIndex = 23;
@@ -113,7 +113,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Himalaya", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(13, 172);
+            this.label3.Location = new System.Drawing.Point(13, 142);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(34, 16);
             this.label3.TabIndex = 22;
@@ -123,7 +123,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Himalaya", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(13, 128);
+            this.label2.Location = new System.Drawing.Point(13, 113);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(134, 16);
             this.label2.TabIndex = 21;
@@ -141,21 +141,21 @@
             // 
             // ücrettb
             // 
-            this.ücrettb.Location = new System.Drawing.Point(174, 300);
+            this.ücrettb.Location = new System.Drawing.Point(174, 216);
             this.ücrettb.Name = "ücrettb";
             this.ücrettb.Size = new System.Drawing.Size(121, 20);
             this.ücrettb.TabIndex = 19;
             // 
             // yaştb
             // 
-            this.yaştb.Location = new System.Drawing.Point(174, 165);
+            this.yaştb.Location = new System.Drawing.Point(174, 135);
             this.yaştb.Name = "yaştb";
             this.yaştb.Size = new System.Drawing.Size(121, 20);
             this.yaştb.TabIndex = 17;
             // 
             // telnotb
             // 
-            this.telnotb.Location = new System.Drawing.Point(174, 125);
+            this.telnotb.Location = new System.Drawing.Point(174, 110);
             this.telnotb.Name = "telnotb";
             this.telnotb.Size = new System.Drawing.Size(121, 20);
             this.telnotb.TabIndex = 16;
@@ -167,6 +167,11 @@
             this.adsoyadtb.Name = "adsoyadtb";
             this.adsoyadtb.Size = new System.Drawing.Size(121, 20);
             this.adsoyadtb.TabIndex = 15;
+            // 
+            // kayitekleDataSet
+            // 
+            this.kayitekleDataSet.DataSetName = "kayitekleDataSet";
+            this.kayitekleDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
             // kangrubucb
             // 
@@ -180,7 +185,7 @@
             "AB-",
             "0+",
             "0-"});
-            this.kangrubucb.Location = new System.Drawing.Point(174, 207);
+            this.kangrubucb.Location = new System.Drawing.Point(174, 163);
             this.kangrubucb.Name = "kangrubucb";
             this.kangrubucb.Size = new System.Drawing.Size(121, 21);
             this.kangrubucb.TabIndex = 29;
@@ -189,43 +194,45 @@
             // 
             this.button3.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button3.Location = new System.Drawing.Point(270, 359);
+            this.button3.Location = new System.Drawing.Point(397, 161);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(102, 41);
+            this.button3.Size = new System.Drawing.Size(118, 41);
             this.button3.TabIndex = 32;
             this.button3.Text = "GERİ";
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(4, 348);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(72, 52);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 34;
-            this.pictureBox1.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(206, 348);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(58, 52);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 35;
-            this.pictureBox2.TabStop = false;
-            // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label7.Location = new System.Drawing.Point(141, 9);
+            this.label7.Location = new System.Drawing.Point(33, 9);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(525, 39);
             this.label7.TabIndex = 36;
             this.label7.Text = "ÜYE GÜNCELLEME VE SİLME";
+            // 
+            // kayitekleDataSetBindingSource
+            // 
+            this.kayitekleDataSetBindingSource.DataSource = this.kayitekleDataSet;
+            this.kayitekleDataSetBindingSource.Position = 0;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(397, 114);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 41);
+            this.button2.TabIndex = 38;
+            this.button2.Text = "SİL";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // kayitekleDataSetBindingSource1
+            // 
+            this.kayitekleDataSetBindingSource1.DataSource = this.kayitekleDataSet;
+            this.kayitekleDataSetBindingSource1.Position = 0;
             // 
             // dataGridView1
             // 
@@ -233,21 +240,14 @@
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(301, 81);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 242);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(494, 272);
-            this.dataGridView1.TabIndex = 37;
-            // 
-            // kayitekleDataSet
-            // 
-            this.kayitekleDataSet.DataSetName = "kayitekleDataSet";
-            this.kayitekleDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // kayitekleDataSetBindingSource
-            // 
-            this.kayitekleDataSetBindingSource.DataSource = this.kayitekleDataSet;
-            this.kayitekleDataSetBindingSource.Position = 0;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(512, 252);
+            this.dataGridView1.TabIndex = 39;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEnter);
             // 
             // guncellemesilme
             // 
@@ -255,11 +255,10 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(587, 506);
             this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.kangrubucb);
             this.Controls.Add(this.button1);
@@ -277,11 +276,10 @@
             this.Name = "guncellemesilme";
             this.Text = "guncellemesilme";
             this.Load += new System.EventHandler(this.guncellemesilme_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.kayitekleDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.kayitekleDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kayitekleDataSetBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -303,11 +301,11 @@
         private System.Windows.Forms.TextBox adsoyadtb;
         private System.Windows.Forms.ComboBox kangrubucb;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.BindingSource kayitekleDataSetBindingSource;
         private kayitekleDataSet kayitekleDataSet;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.BindingSource kayitekleDataSetBindingSource1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- WindowsFormsApp1/guncellemesilme.cs
+++ WindowsFormsApp1/guncellemesilme.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.Sql;
 using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace WindowsFormsApp1
 {
@@ -21,17 +22,16 @@ namespace WindowsFormsApp1
         }
 
         SqlConnection bagla = new SqlConnection("Data Source=DELL\\SQLEXPRESS;Initial Catalog=kayitekle;Integrated Security=True");
-
         private void uyeler()
         {
             bagla.Open();
-            string query = "select * from kayitekle";
+            string query = "select *from kayitekle";
             SqlDataAdapter sda = new SqlDataAdapter(query, bagla);
+            SqlCommandBuilder builder = new SqlCommandBuilder();
             var ds = new DataSet();
             sda.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
             bagla.Close();
-
-            
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -46,21 +46,80 @@ namespace WindowsFormsApp1
             uyeler();
         }
         int key = 0;
-        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        private void dataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e)
         {
-          
-                adsoyadtb.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
-                telnotb.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
-                yaştb.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
-                kangrubucb.Text = dataGridView1.SelectedRows[0].Cells[4].Value.ToString();
-                cinsiyetcb.Text = dataGridView1.SelectedRows[0].Cells[5].Value.ToString();
-                ücrettb.Text = dataGridView1.SelectedRows[0].Cells[6].Value.ToString();
+            adsoyadtb.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            telnotb.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            yaştb.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            kangrubucb.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            cinsiyetcb.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            ücrettb.Text = dataGridView1.CurrentRow.Cells[5].Value.ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string adsoyad = adsoyadtb.Text;
+
+            if (string.IsNullOrEmpty(adsoyad))
+            {
+                MessageBox.Show("Silinecek üyenin adını ve soyadını giriniz.");
+            }
+            else
+            {
+                try
+                {
+                    string sorgu = "DELETE FROM kayitekle WHERE adsoyad = @adsoyad";
+                    SqlCommand komut = new SqlCommand(sorgu, bagla);
+                    komut.Parameters.AddWithValue("@adsoyad", adsoyad);
+                    bagla.Open();
+                    komut.ExecuteNonQuery();
+                    MessageBox.Show("Üye başarıyla silindi.");
+                    bagla.Close();
+                    uyeler();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
             }
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (key == 0)
+            {
+                MessageBox.Show("Güncellenecek üyeyi seçiniz.");
+            }
+            else
+            {
+                try
+                {
+                    string sorgu = "UPDATE kayitekle SET adsoyad=@adsoyad, telno=@telno, yaş=@yaş, kangrubu=@kangrubu, cinsiyet=@cinsiyet, ücret=@ücret WHERE id=@id";
+                    SqlCommand komut = new SqlCommand(sorgu, bagla);
+                    komut.Parameters.AddWithValue("@adsoyad", adsoyadtb.Text);
+                    komut.Parameters.AddWithValue("@telno", telnotb.Text);
+                    komut.Parameters.AddWithValue("@yaş", yaştb.Text);
+                    komut.Parameters.AddWithValue("@kangrubu", kangrubucb.Text);
+                    komut.Parameters.AddWithValue("@cinsiyet", cinsiyetcb.Text);
+                    komut.Parameters.AddWithValue("@ücret", ücrettb.Text);
+                    komut.Parameters.AddWithValue("@id", key);
+                    bagla.Open();
+                    komut.ExecuteNonQuery();
+                    bagla.Close();
+                    uyeler();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
 
+        }
 
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
 
-
-
+        }
     }
+}
